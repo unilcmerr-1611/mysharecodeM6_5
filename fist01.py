2a98c6022b77f5bb548d0e80a9e9c8734608f67a
unilcmerr-1611 <164029@wsk.ac.th>
2026-06-16 04:09:10 +0000

    fist01.py

---
+++ fist01.py
@@ -0,0 +1,6 @@
+print("โปรแกรมคำนวณหาพื้นที่ของสามเหลี่ยม")
+h=float(input("enter hight:"))
+b=float(input("enter base:"))
+area=0.5*h*b
+print("พื้นที่ของสามเหลี่ยมเท่ากับ",area)
+print("จัดทำโดย นางสาวพรทิพย์ บูรณนัฎ เลขที่ 15")
